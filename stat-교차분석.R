30e091775677c5abbcf5310b8b7452d5d4892ff9
LeeYeonDong <LeeYeonDong@users.noreply.github.com>
2020-05-12 22:15:08 +0900

    Add files via upload

--- stat-교차분석.R
+++ stat-교차분석.R
@@ -1,20 +1,23 @@
-rawdata  <- read.csv("D:/사회가족실설문지/설문지.csv", header=TRUE)
+library(dplyr)
+library(gmodels)
 
+rawdata <- read.csv("D:/사회가족실설문지/설문지.csv", header=TRUE)
 
 
 rawdata <- rawdata %>% filter(w06psvy == 1)
 rawdata <- subset(rawdata, select=-c(pid,income_month_range))
 rawdata <- rawdata %>% filter(intention == 0 |intention == 1)
 
-
-table.df <- data.frame()
-corr.df <- data.frame()
-pvalue.m <- matrix()
+rawdata_dim <- rawdata %>% dim()
 
 
+pvalue.c <- c()
 
-for(i in 1:945){
+
+for(i in 1:rawdata_dim[2]){
   tryCatch({
-    table.df <- CrossTable(rawdata[,i],rawdata$intention, expected=TRUE, chisq=TRUE,format =c("SPSS"))
-    pvalue.m[i] <- table.df$CST$p.value}, error = function(e) print="error")
+    table.df <- CrossTable(rawdata[,i],rawdata$intention, expected=TRUE, chisq=TRUE)
+    pvalue.c[i] <- table.df$chisq$p.value}, error = function(e) print="error")
 }
+
+pvalue.c %>% View()
